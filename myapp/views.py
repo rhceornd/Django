e7915b709eebff11abf079fdff17e39c509fb764
Won Doyoung <dywon93@naver.com>
2023-02-21 11:39:42 +0900

    edit view.py 상세페이지보기

--- myapp/views.py
+++ myapp/views.py
@@ -7,27 +7,41 @@ topics = [
     {'id':2, 'title':'view 알아보기', 'body':'View is ..'},
     {'id':3, 'title':'Model 알아보기', 'body':'Model is ..'}
 ]
-# 클라이언트로 정보를 전달하는 함수
-def index(request):
+
+def HTMLTemplate(articleTag):
     global topics
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'  # <li>routing</li> 등을 대체
-    return HttpResponse(f"""
+
+    return f'''
     <html>
     <body>
-        <h1>Django</h1>
+        <h1><a href="/">Django</a></h1>
         <ol>
             {ol}
         </ol>
-        <h2>Welcome</h2>
-        Hello, Django
+        {articleTag}
     </body>
-    </html>    
-    """) # reponse through http
+    </html>     
+    '''
+
+# 클라이언트로 정보를 전달하는 함수
+def index(request):
+    article = """
+    <h2>Welcome</h2>
+    Hello, Django
+    """
+    return HttpResponse(HTMLTemplate(article)) # reponse through http
 
 def read(request, id):
-    return HttpResponse("Read!" + id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+
+    return HttpResponse(HTMLTemplate(article))
 
 def create(request):
     return HttpResponse("Create")
