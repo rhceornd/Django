6b96bc21ba0e8bd74f96f89f589b447a6b6ad412
Won Doyoung <dywon93@naver.com>
2023-02-21 16:52:04 +0900

    add delete func

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,10 +7,11 @@ from django.contrib import admin
 from django.urls import path, include
 from myapp import views
 
-
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
-    path('read/<id>/', views.read)
+    path('read/<id>/', views.read),
+
+    path('delete/', views.delete)
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 import random
 
-# Create your views here.
+nextId = 4
+
 topics = [
     {'id':1, 'title':'routing 알아보기', 'body':'Routing is ..'},
     {'id':2, 'title':'view 알아보기', 'body':'View is ..'},
-    {'id':3, 'title':'Model 알아보기', 'body':'Model is ..'}
+    {'id':3, 'title':'Model 알아보기', 'body':'Model is .. 블라블라'}
 ]
 
-def HTMLTemplate(articleTag):
+# HTML 형식
+def HTMLTemplate(articleTag, id=None):
     global topics
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id", value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li>
+        '''
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'  # <li>routing</li> 등을 대체
@@ -22,6 +35,11 @@ def HTMLTemplate(articleTag):
             {ol}
         </ol>
         {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+            {contextUI}
+        </ul>
+
     </body>
     </html>     
     '''
@@ -31,6 +49,7 @@ def index(request):
     article = """
     <h2>Welcome</h2>
     Hello, Django
+    This is DY homepage
     """
     return HttpResponse(HTMLTemplate(article)) # reponse through http
 
@@ -40,13 +59,48 @@ def read(request, id):
     for topic in topics:
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article, id))
 
-    return HttpResponse(HTMLTemplate(article))
-
+# title, body를 서버에 원하는 패스로 보내기 위해서 form tag로 감싸야함.
+# form 에 method를 따로 지정하지 않으면 get 방식으로 요청
+@csrf_exempt
 def create(request):
-    return HttpResponse("Create")
+    global nextId
+    # print("request method :", request.method)
+    if request.method == 'GET':
+        article = """
+            <form action="/create/" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        """
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        print(request.POST)
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id":nextId, "title":title, "body":body}
+        topics.append(newTopic)
+        url = '/read/'+ str(nextId)
+        nextId += 1
+
+        return redirect(url)
 
 
+@csrf_exempt
+def delete(request):
+    global  topics
+    if request.method == 'POST':
+        id = request.POST['id']
+        # print('id :', id)
+
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics
+        return redirect('/')
 
 
 
